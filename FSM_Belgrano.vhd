e52645d402d65e2256c00add170cbdc772a6d895
JinnKaY <menendez91@live.com.ar>
2018-10-23 13:47:11 -0300

    Dos maquinas

--- FSM_Belgrano.vhd
+++ FSM_Belgrano.vhd
@@ -21,11 +21,12 @@ architecture FSM_Belgrano_arq of FSM_Belgrano is
 	constant N_SM: natural := 16;
 	constant N_rutas: natural := 6;	
 	
-	type rutas is (RUTA0,RUTA1,RUTA2,RUTA3,RUTA4,RUTA5,RUTA6);
+	type rutas is (RUTA0,RUTA1,RUTA2,RUTA3,RUTA4,RUTA5,RUTA6,RUTA7,RUTA8,RUTA9,RUTA10,RUTA11,RUTA12,RUTA13,RUTA14);
 	type circuitos_t is (CIRC1,CIRC2,CIRC3,CIRC4,CIRC5,CIRC6,CIRC7,CIRC8,CIRC9,CIRC10,CIRC11,CIRC12);
 	type semaforos_t is (SEM_A,SEM_B,SEM_C,SEM_D,SEM_E,SEM_F,SEM_G,SEM_H,SEM_I,SEM_J,SEM_K,SEM_L,SEM_M,SEM_N,SEM_O,SEM_P,SEM_Q);
 	
-	signal ruta_actual: rutas;
+	signal ruta_ascendente: rutas;
+	signal ruta_descendente: rutas;
 	
 	constant OCUPADO: std_logic := '0';
 	constant LIBRE: std_logic := '1';
@@ -38,12 +39,13 @@ architecture FSM_Belgrano_arq of FSM_Belgrano is
 	constant BARRERA_ALTA: std_logic := '1';	
 	
 	signal habilitacion: std_logic_vector(N_rutas downto 0);
+	signal PaN_ASC: std_logic := '0';
+	signal PaN_DES: std_logic := '0';
 	
 begin	
 	
 	habilitacion(rutas'pos(RUTA0)) <= '1';
 	habilitacion(rutas'pos(RUTA1)) <= '1' when 
-						 Circuito_Via(circuitos_t'pos(CIRC9)) = LIBRE and
 						 Circuito_Via(circuitos_t'pos(CIRC1)) = OCUPADO and 
 						 Circuito_Via(circuitos_t'pos(CIRC3)) = LIBRE and 
 						 Circuito_Via(circuitos_t'pos(CIRC5)) = LIBRE and 
@@ -94,39 +96,51 @@ begin
 						 Circuito_Via(circuitos_t'pos(CIRC11)) = LIBRE and 
 						 Circuito_Via(circuitos_t'pos(CIRC12)) = LIBRE else '0';
 		
-	process(clock, reset)
+	ASCENDENTE: process(clock, reset)
 	begin
 		if (reset = '1') then
-			ruta_actual <= RUTA0;
+			ruta_ascendente <= RUTA0;
 		elsif (clock'event and clock='1') then
 			if (ruta = 1 and habilitacion(rutas'pos(RUTA1)) = '1') then
-				ruta_actual <= RUTA1;
+				ruta_ascendente <= RUTA1;
 			end if;
 			if (ruta = 2 and habilitacion(rutas'pos(RUTA2)) = '1') then
-				ruta_actual <= RUTA2;
-			end if;
-			if (ruta = 3 and habilitacion(rutas'pos(RUTA3)) = '1') then
-				ruta_actual <= RUTA3;
-			end if;
-			if (ruta = 4 and habilitacion(rutas'pos(RUTA4)) = '1') then
-				ruta_actual <= RUTA4;
+				ruta_ascendente <= RUTA2;
 			end if;
 			if (ruta = 5 and habilitacion(rutas'pos(RUTA5)) = '1') then
-				ruta_actual <= RUTA5;
+				ruta_ascendente <= RUTA5;
 			end if;
 			if (ruta = 6 and habilitacion(rutas'pos(RUTA6)) = '1') then
-				ruta_actual <= RUTA6;
+				ruta_ascendente <= RUTA6;
 			end if;
 		end if;
-	end process;	
+	end process ASCENDENTE;	
 	
-	 process (ruta_actual,clock)
-	 begin
+	DESCENDENTE: process(clock, reset)
+	begin
+		if (reset = '1') then
+			ruta_descendente <= RUTA0;
+		elsif (clock'event and clock='1') then
+			if (ruta = 3 and habilitacion(rutas'pos(RUTA3)) = '1') then
+				ruta_descendente <= RUTA3;
+			end if;
+			if (ruta = 4 and habilitacion(rutas'pos(RUTA4)) = '1') then
+				ruta_descendente <= RUTA4;
+			end if;
+			if (ruta = 5 and habilitacion(rutas'pos(RUTA5)) = '1') then
+				ruta_descendente <= RUTA5;
+			end if;
+			if (ruta = 6 and habilitacion(rutas'pos(RUTA6)) = '1') then
+				ruta_descendente <= RUTA6;
+			end if;
+		end if;
+	end process DESCENDENTE;
+	
+	RUTA_ASC: process (ruta_ascendente,clock)
+	begin
 		if (clock'event and clock ='1') then
-		case ruta_actual is
+		case ruta_ascendente is
 			when RUTA0 => -- Ruta no asignada
-				-- Siempre asignar TODAS las salidas
-				-- Crear estados combinados
 				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_C)) <= ROJO;
@@ -144,7 +158,7 @@ begin
 				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;
 				Maquina <= NORMAL;
-				PaN <= BARRERA_BAJA;
+				PaN_ASC <= BARRERA_BAJA;
 			when RUTA1 => 
 				Semaforos(semaforos_t'pos(SEM_A)) <= VERDE;
 				Semaforos(semaforos_t'pos(SEM_B)) <= VERDE;
@@ -156,7 +170,7 @@ begin
 				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;			
 				Maquina <= NORMAL;
-				PaN <= BARRERA_BAJA;
+				PaN_ASC <= BARRERA_BAJA;
 				--contador
 			when RUTA2 => 
 				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
@@ -169,8 +183,85 @@ begin
 				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;			
 				Maquina <= NORMAL;
-				PaN <= BARRERA_BAJA;
-			when RUTA3 => 
+				PaN_ASC <= BARRERA_BAJA;
+			when RUTA3 => Maquina <= NORMAL;
+			when RUTA4 => Maquina <= NORMAL;
+			when RUTA5 => Maquina <= NORMAL;
+				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_C)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_D)) <= VERDE;
+				Semaforos(semaforos_t'pos(SEM_E)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_H)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_I)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_J)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_K)) <= ROJO;	
+				Semaforos(semaforos_t'pos(SEM_L)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_M)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_N)) <= VERDE;
+				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_P)) <= VERDE;
+				Maquina <= REVERSA;
+				PaN_ASC <= BARRERA_BAJA;
+			when RUTA6 =>
+				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_C)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_D)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_E)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_H)) <= VERDE;
+				Semaforos(semaforos_t'pos(SEM_I)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_J)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_K)) <= ROJO;	
+				Semaforos(semaforos_t'pos(SEM_L)) <= AMARILLO;
+				Semaforos(semaforos_t'pos(SEM_M)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;
+				Maquina <= REVERSA;
+				PaN_ASC <= BARRERA_BAJA;
+			when RUTA7 => Maquina <= NORMAL;	
+			when RUTA8 => Maquina <= NORMAL;
+			when RUTA9 => Maquina <= NORMAL;
+			when RUTA10 => Maquina <= NORMAL;
+			when RUTA11 => Maquina <= NORMAL;
+			when RUTA12 => Maquina <= NORMAL;
+			when RUTA13 => Maquina <= NORMAL;
+			when RUTA14 => Maquina <= NORMAL;
+		end case;
+		end if;		
+	end process RUTA_ASC;
+	
+	RUTA_DES: process (ruta_descendente,clock)
+	begin
+		if (clock'event and clock ='1') then
+		case ruta_descendente is
+			when RUTA0 => -- Ruta no asignada
+				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_C)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_D)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_E)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_H)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_I)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_J)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_K)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_L)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_M)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;
+				Maquina <= NORMAL;
+				PaN_DES <= BARRERA_BAJA;
+			when RUTA1 => Maquina <= NORMAL;
+			when RUTA2 => Maquina <= NORMAL;
+			when RUTA3 => Maquina <= NORMAL;
 				Semaforos(semaforos_t'pos(SEM_F)) <= VERDE;
 				Semaforos(semaforos_t'pos(SEM_G)) <= VERDE;
 				Semaforos(semaforos_t'pos(SEM_H)) <= VERDE;
@@ -181,8 +272,9 @@ begin
 				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;			
 				Maquina <= NORMAL;
-				PaN <= BARRERA_BAJA;
-			when RUTA4 =>
+				PaN_DES <= BARRERA_BAJA;
+				--contador
+			when RUTA4 => 
 				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_H)) <= ROJO;
@@ -193,7 +285,7 @@ begin
 				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;			
 				Maquina <= NORMAL;
-				PaN <= BARRERA_ALTA;
+				PaN_DES <= BARRERA_ALTA;
 			when RUTA5 =>
 				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
@@ -203,36 +295,54 @@ begin
 				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_H)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_I)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_J)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_K)) <= ROJO;	
 				Semaforos(semaforos_t'pos(SEM_L)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_M)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_N)) <= VERDE;
+				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_P)) <= VERDE;
 				Maquina <= REVERSA;
-				PaN <= BARRERA_BAJA;
+				PaN_DES <= BARRERA_BAJA;
 			when RUTA6 =>
 				Semaforos(semaforos_t'pos(SEM_A)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_B)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_C)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_D)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_E)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_F)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_G)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_H)) <= VERDE;
 				Semaforos(semaforos_t'pos(SEM_I)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_J)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_K)) <= ROJO;	
-				Semaforos(semaforos_t'pos(SEM_L)) <= VERDE;
+				Semaforos(semaforos_t'pos(SEM_L)) <= AMARILLO;
 				Semaforos(semaforos_t'pos(SEM_M)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_N)) <= ROJO;
+				Semaforos(semaforos_t'pos(SEM_O)) <= ROJO;
 				Semaforos(semaforos_t'pos(SEM_P)) <= ROJO;
 				Maquina <= REVERSA;
-				PaN <= BARRERA_BAJA;
+				PaN_DES <= BARRERA_BAJA;
+			when RUTA7 => Maquina <= NORMAL;
+			when RUTA8 => Maquina <= NORMAL;
+			when RUTA9 => Maquina <= NORMAL;
+			when RUTA10 => Maquina <= NORMAL;
+			when RUTA11 => Maquina <= NORMAL;
+			when RUTA12 => Maquina <= NORMAL;
+			when RUTA13 => Maquina <= NORMAL;
+			when RUTA14 => Maquina <= NORMAL;
 		end case;
+		end if;		
+	end process RUTA_DES;
+	
+	BARRERAS: process(clock, reset)
+	begin
+		if (reset = '1') then
+			PaN <= BARRERA_BAJA;
+		elsif (clock'event and clock='1') then
+			PaN <= PaN_ASC and PaN_DES;
 		end if;
-
-		--Salida <= Semaforos; -- 0 rojo 1 Naranja -1 Verde
-		--Salida_M <= Maquina;
-		--Salida_P <= PaN;
-		
-	end process;
+	end process BARRERAS;
 	
 end architecture;
--- FSM_Belgrano_tb.vhd
+++ FSM_Belgrano_tb.vhd
@@ -23,20 +23,42 @@ architecture FSM_Belgrano_tb_arq of FSM_Belgrano_tb is
 	
 	constant N_tb: natural := 12;
 	constant N_SM_tb: natural := 16;
-	constant delay: time := 500 ns;
+	constant delay: time := 100 ns;
 	
-	signal Circuito_Via_tb: std_logic_vector(N_tb-1 downto 0) := (N_tb-1 downto 0 => '0');
+	signal Circuito_Via_tb: std_logic_vector(N_tb-1 downto 0) := (N_tb-1 downto 0 => '1');
 	signal ruta_tb: integer := 0;
 	signal clock_tb: std_logic := '0';
 	signal reset_tb: std_logic := '0';
 	signal Semaforos_tb: SEMAFORO;
 	signal Maquina_tb: std_logic;
 	signal PaN_tb: std_logic;
+	
+	type rutas is (RUTA0,RUTA1,RUTA2,RUTA3,RUTA4,RUTA5,RUTA6,RUTA7,RUTA8,RUTA9,RUTA10,RUTA11,RUTA12,RUTA13,RUTA14);
+	type circuitos_t is (CIRC1,CIRC2,CIRC3,CIRC4,CIRC5,CIRC6,CIRC7,CIRC8,CIRC9,CIRC10,CIRC11,CIRC12);
+	type semaforos_t is (SEM_A,SEM_B,SEM_C,SEM_D,SEM_E,SEM_F,SEM_G,SEM_H,SEM_I,SEM_J,SEM_K,SEM_L,SEM_M,SEM_N,SEM_O,SEM_P,SEM_Q);
+
+	constant OCUPADO: std_logic := '0';
+	constant LIBRE: std_logic := '1';
+	constant ROJO: SM_ESTADO := "00";		--  0
+	constant AMARILLO: SM_ESTADO := "01";	-- +1
+	constant VERDE: SM_ESTADO := "11";		-- -1
+	constant NORMAL: std_logic := '0';
+	constant REVERSA: std_logic := '1';
+	constant BARRERA_BAJA: std_logic := '0';
+	constant BARRERA_ALTA: std_logic := '1';
+	
 begin
 	clock_tb <= not clock_tb after 10 ns;
 	reset_tb <= '1' after 5000 ns;
 
-	RUTAS: process 
+	CVS: process 
+	begin
+		Circuito_Via_tb(circuitos_t'pos(CIRC1)) <= OCUPADO;
+		wait for delay;
+		Circuito_Via_tb(circuitos_t'pos(CIRC1)) <= LIBRE;
+	end process CVS;
+	
+	RUTAS_gen: process 
 	begin
 		wait for delay;
 		ruta_tb <= 1;
@@ -51,7 +73,7 @@ begin
 		wait for delay;
 		ruta_tb <= 6;
 		wait for 1000 ns;
-	end process RUTAS;
+	end process RUTAS_gen;
 
 	DUT: FSM_Belgrano
 		port map(
